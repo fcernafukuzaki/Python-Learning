b3bc392ad2c05b8303816bd0430b5ac6bd5f5dc7
Francisco Cerna Fukuzaki <fcernaf@gmail.com>
2019-12-18 20:17:38 -0500

    Transposición de producto interno de matrices

--- Algebra_Lineal_Matrices.py
+++ Algebra_Lineal_Matrices.py
@@ -13,6 +13,7 @@ multiplicacion_matriz_matriz = matriz * matriz
 print(multiplicacion_matriz_matriz)
 
 ## Producto interno
+print('')
 print('P R O D U C T O   I N T E R N O')
 print('Matriz por Vector')
 producto_interno = matriz.dot(vector)
@@ -25,3 +26,21 @@ print(producto_interno)
 print('Matriz por Matriz')
 multiplicacion_matriz_matriz = np.dot(matriz, matriz)
 print(multiplicacion_matriz_matriz)
+
+## Producto interno
+print('')
+print('T R A N S P O S I C I Ó N   D E   P R O D U C T O   I N T E R N O   D E   M A T R I C E S')
+matriz_a = np.array([[2, 3], [5, 7], [11, 13]])
+matriz_b = np.array([[1, 3], [2,1]])
+print('Matriz A: {}'.format(matriz_a))
+print('Matriz B: {}'.format(matriz_b))
+print('Resultado 1:')
+producto_matriz_matriz = np.dot(matriz_a, matriz_b)
+#producto_matriz_matriz = matriz_a.dot(matriz_b).T
+producto_matriz_matriz_t = producto_matriz_matriz.T
+print(producto_matriz_matriz_t)
+print('Resultado 2:')
+producto_matriz_t_matriz_t = np.dot(matriz_b.T, matriz_a.T)
+print(producto_matriz_t_matriz_t)
+print('Validación de matrices:')
+print(producto_matriz_matriz_t == producto_matriz_t_matriz_t)
